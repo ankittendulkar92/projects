8adf60a96b72339364285b47f6d706e1081716f4
Ankit Tendulkar <ankittendulkar92@gmail.com>
2017-09-25 19:16:16 -0400

    Add files via upload

---
+++ movie recommendation/movie-similarities-1m.py
@@ -0,0 +1,102 @@
+import sys
+from pyspark import SparkConf, SparkContext
+from math import sqrt
+
+#aws s3 cp s3://sundog-spark/MovieSimilarities1M.py ./
+#aws s3 sp c3://sundog-spark/ml-1m/movies.dat ./
+#spark-submit --executor-memory 1g MovieSimilarities1M.py 260
+
+def loadMovieNames():
+    movieNames = {}
+    with open("movies.dat") as f:
+        for line in f:
+            fields = line.split("::")
+            movieNames[int(fields[0])] = fields[1].decode('ascii', 'ignore')
+    return movieNames
+
+def makePairs((user, ratings)):
+    (movie1, rating1) = ratings[0]
+    (movie2, rating2) = ratings[1]
+    return ((movie1, movie2), (rating1, rating2))
+
+def filterDuplicates( (userID, ratings) ):
+    (movie1, rating1) = ratings[0]
+    (movie2, rating2) = ratings[1]
+    return movie1 < movie2
+
+def computeCosineSimilarity(ratingPairs):
+    numPairs = 0
+    sum_xx = sum_yy = sum_xy = 0
+    for ratingX, ratingY in ratingPairs:
+        sum_xx += ratingX * ratingX
+        sum_yy += ratingY * ratingY
+        sum_xy += ratingX * ratingY
+        numPairs += 1
+
+    numerator = sum_xy
+    denominator = sqrt(sum_xx) * sqrt(sum_yy)
+
+    score = 0
+    if (denominator):
+        score = (numerator / (float(denominator)))
+
+    return (score, numPairs)
+
+
+conf = SparkConf()
+sc = SparkContext(conf = conf)
+
+print("\nLoading movie names...")
+nameDict = loadMovieNames()
+
+data = sc.textFile("s3n://sundog-spark/ml-1m/ratings.dat")
+
+# Map ratings to key / value pairs: user ID => movie ID, rating
+ratings = data.map(lambda l: l.split("::")).map(lambda l: (int(l[0]), (int(l[1]), float(l[2]))))
+
+# Emit every movie rated together by the same user.
+# Self-join to find every combination.
+ratingsPartitioned = ratings.partitionBy(100)
+joinedRatings = ratingsPartitioned.join(ratingsPartitioned)
+
+
+# Filter out duplicate pairs
+uniqueJoinedRatings = joinedRatings.filter(filterDuplicates)
+
+#  (movie1, movie2) pairs.
+moviePairs = uniqueJoinedRatings.map(makePairs).partitionBy(100)
+
+# Now collect all ratings for each movie pair and compute similarity
+moviePairRatings = moviePairs.groupByKey()
+
+# compute similarities.
+moviePairSimilarities = moviePairRatings.mapValues(computeCosineSimilarity).persist()
+
+# Save the results 
+moviePairSimilarities.sortByKey()
+moviePairSimilarities.saveAsTextFile("movie-sims")
+
+#
+if (len(sys.argv) > 1):
+
+    scoreThreshold = 0.97
+    coOccurenceThreshold = 1000
+
+    movieID = int(sys.argv[1])
+
+    # Filter for movies with thresholds above
+    filteredResults = moviePairSimilarities.filter(lambda((pair,sim)): \
+        (pair[0] == movieID or pair[1] == movieID) \
+        and sim[0] > scoreThreshold and sim[1] > coOccurenceThreshold)
+
+    # Sort by quality score.
+    results = filteredResults.map(lambda((pair,sim)): (sim, pair)).sortByKey(ascending = False).take(10)
+
+    print("Top 10 similar movies for " + nameDict[movieID])
+    for result in results:
+        (sim, pair) = result
+        # Display the similarity result that isn't the movie we're looking at
+        similarMovieID = pair[0]
+        if (similarMovieID == movieID):
+            similarMovieID = pair[1]
+        print(nameDict[similarMovieID] + "\tscore: " + str(sim[0]) + "\tstrength: " + str(sim[1]))
